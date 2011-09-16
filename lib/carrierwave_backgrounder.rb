491f635d03dbab8c8fe0149a57e7085f458fb986
Robert Rouse <robert.e.rouse@gmail.com>
2011-09-16 15:10:56 -0500

    Remove this for Rails 2

--- lib/carrierwave_backgrounder.rb
+++ lib/carrierwave_backgrounder.rb
@@ -7,17 +7,3 @@ module CarrierWave
     end
   end
 end
-
-if defined?(Rails)
-  module CarrierWave
-    module Backgrounder
-      class Railtie < Rails::Railtie
-        initializer "carrierwave_backgrounder.active_record" do
-          ActiveSupport.on_load :active_record do
-            require 'backgrounder/orm/activerecord'
-          end
-        end
-      end
-    end
-  end
-end
